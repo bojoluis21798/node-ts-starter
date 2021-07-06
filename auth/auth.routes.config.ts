e755aba39a4e833405fa37feef6772269353e71b
bojoluis21798 <bojoluis21798@gmail.com>
2021-07-06 11:43:44 +0800

    Create refresh token

--- auth/auth.routes.config.ts
+++ auth/auth.routes.config.ts
@@ -3,6 +3,7 @@ import authController from "./controllers/auth.controller";
 import authMiddleware from "./middlewares/auth.middleware";
 import express from "express";
 import bodyValidationMiddleware from "../common/middlewares/body.validation.middleware";
+import jwtMiddleware from "./middlewares/jwt.middleware";
 import { body } from "express-validator";
 
 export class AuthRoutes extends CommonRoutesConfig {
@@ -16,6 +17,13 @@ export class AuthRoutes extends CommonRoutesConfig {
       body("password").isString(),
       bodyValidationMiddleware.verifyBodyFieldsErrors,
       authMiddleware.verifyUserPassword,
+      authController.createJWT,
+    ]);
+
+    this.app.post("/auth/refresh-token", [
+      jwtMiddleware.validJWTNeeded,
+      jwtMiddleware.verifyRefreshBodyField,
+      jwtMiddleware.validRefreshNeeded,
       authController.createJWT,
     ]);
 
